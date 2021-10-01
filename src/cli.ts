494f2452af10e6a1b249e049025b7b0a9d116d41
Enes Genç <enesgenc4661@gmail.com>
2021-10-01 23:20:31 +0300

    feat: switch to Commander

--- src/cli.ts
+++ src/cli.ts
@@ -1,16 +1,30 @@
 #!/usr/bin/env node
 
-import { run } from '@oclif/command';
-import oclifErrorHandler from '@oclif/errors/handle';
-import flush from '@oclif/command/flush';
+import { Command } from 'commander';
+import { readFile } from 'fs/promises';
 
-async function main() {
-	try {
-		await run();
-		await flush();
-	} catch (error) {
-		oclifErrorHandler(error as Error);
-	}
-}
+import newCmd from '#commands/new.js';
+import generateCmd from '#commands/generate.js';
 
-void main();
+const sapphire = new Command();
+const packageJson = JSON.parse(await readFile(`${process.cwd()}/package.json`, 'utf8'));
+
+sapphire.name('sapphire').version(packageJson.version);
+
+sapphire
+	.command('new')
+	.description('creates a new Sapphire project')
+	.alias('n')
+	.argument('[name]', 'project name')
+	.option('-v, --verbose')
+	.action(newCmd);
+
+sapphire
+	.command('generate')
+	.description('generates a component/piece')
+	.alias('g')
+	.argument('<component>', 'component/piece name')
+	.argument('<name>', 'file name')
+	.action(generateCmd);
+
+sapphire.parse(process.argv);
--- src/lib/types/oclifErrorHandler.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@oclif/errors/handle' {
-	import type { PrettyPrintableError } from '@oclif/errors/lib/errors/pretty-print';
-	import type { OclifError } from '@oclif/errors/lib/errors/cli';
-
-	const handle: (err: Error & Partial<PrettyPrintableError> & Partial<OclifError>) => void;
-
-	export default handle;
-}
