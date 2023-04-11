3189772a1b85ecc2bad2949ed8d339ff0541cebf
Jeroen Claassens <support@favware.tech>
2023-04-12 01:08:37 +0200

    refactor(CreateFileFromTemplate): better internal code naming

--- src/functions/CreateFileFromTemplate.ts
+++ src/functions/CreateFileFromTemplate.ts
@@ -13,52 +13,59 @@ export async function CreateFileFromTemplate(
 ) {
 	const location = custom ? template : `${templatesFolder}${template}`;
 
-	const output = {} as {
-		f: string;
-		c?: Record<string, string>;
-	};
+	const output = {} as FileOutput;
 
 	if (component) {
-		const [c, f] = await getComponentTemplateWithConfig(location);
+		const [config, templateContent] = await getComponentTemplateWithConfig(location);
 
-		output.c = c;
-		output.f = f;
+		output.config = config;
+		output.templateContent = templateContent;
 	}
 
-	output.f ??= await readFile(location, 'utf8');
+	output.templateContent ??= await readFile(location, 'utf8');
 
-	if (!output.f) {
-		throw new Error("Can't read file.");
+	if (!output.templateContent) {
+		throw new Error(`Couldn't read the template file. Are you sure it exists, the name is correct, and the content is valid?`);
 	}
 
 	if (params) {
 		for (const param of Object.entries(params)) {
-			output.f = output.f.replaceAll(`{{${param[0]}}}`, param[1]);
+			output.templateContent = output.templateContent.replaceAll(`{{${param[0]}}}`, param[1]);
 		}
 	}
 
-	if (!output || (component && (!output.c || !output.c.category))) {
+	if (!output || (component && (!output.config || !output.config.category))) {
 		throw new Error('Invalid template.');
 	}
 
-	const dir = component ? config.locations[output.c!.category] : null;
+	const dir = component ? config.locations[output.config!.category] : null;
 	const ta = component ? target.replace('%L%', dir) : target;
 
 	if (await fileExists(ta)) {
 		throw new Error('Component already exists');
 	}
 
-	await writeFileRecursive(ta, output.f);
+	await writeFileRecursive(ta, output.templateContent);
 
 	return true;
 }
 
-async function getComponentTemplateWithConfig(path: string) {
+/**
+ * Gets the template and the config from a component template
+ * @param path Path to the template
+ * @returns [config, template] The config and the template
+ */
+async function getComponentTemplateWithConfig(path: string): Promise<[config: Record<string, string>, template: string]> {
 	const file = await readFile(path, 'utf8');
 	const fa = file.split(/---(\r\n|\r|\n|)/gm);
 	return [JSON.parse(fa[0]), fa[2]];
 }
 
+/**
+ * Writes a file recursively
+ * @param target Target path
+ * @param data Data to write
+ */
 async function writeFileRecursive(target: string, data: string) {
 	const resolvedTarget = resolve(target);
 	const dir = dirname(resolvedTarget);
@@ -66,4 +73,9 @@ async function writeFileRecursive(target: string, data: string) {
 	await mkdir(dir, { recursive: true });
 
 	return writeFile(resolvedTarget, data);
+}
+
+interface FileOutput {
+	templateContent: string;
+	config?: Record<string, string>;
 }
