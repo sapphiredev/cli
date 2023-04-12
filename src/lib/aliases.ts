c57e9c64efa06af26a9c3a8c1bd4510a00af967e
Williams Bussat <williams@wbussat.fr>
2023-04-12 11:30:10 +0200

    refactor: optimize the logic of "parseCommonHints" function for better readability

---
+++ src/lib/aliases.ts
@@ -0,0 +1,10 @@
+export const commandNames = ['command', 'commands'];
+export const componentCommandNames = ['messagecommand', 'slashcommand', 'contextmenucommand'];
+
+export const interactionHandlerNames = ['interactionhandler', 'interactionhandlers'];
+export const componentInteractionHandlerNames = [
+	'buttoninteractionhandler',
+	'autocompleteinteractionhandler',
+	'modalinteractionhandler',
+	'selectmenuinteractionhandler'
+];
