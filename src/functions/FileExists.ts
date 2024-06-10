4cae50c2890186fe753b2f75d0c528ade84d113e
Enes Genç <61084101+enxg@users.noreply.github.com>
2024-06-10 12:08:25 +0300

    initial commit for rust rewrite

--- src/functions/FileExists.ts
+++
@@ -1,8 +0,0 @@
-import { Result } from '@sapphire/result';
-import { access } from 'node:fs/promises';
-
-export async function fileExists(filePath: string): Promise<boolean> {
-	const result = await Result.fromAsync(() => access(filePath));
-
-	return result.isOk();
-}
